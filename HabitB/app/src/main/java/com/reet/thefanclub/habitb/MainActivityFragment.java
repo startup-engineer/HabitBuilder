2ab966df9fe08d8737bb0e7218f0cea27eafa6a7
Reet Chowdhary <reetchow@bu.edu>
2015-12-05 18:26:17 -0500

    added ListView

---
+++ HabitB/app/src/main/java/com/reet/thefanclub/habitb/MainActivityFragment.java
@@ -0,0 +1,76 @@
+package com.reet.thefanclub.habitb;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "first thing",
+                "Second thing",
+                "third thing",
+                "fourth thing",
+                "fifth thing",
+                "sixth thing",
+                "seventh thing",
+                "first thing",
+                "Second thing",
+                "third thing",
+                "fourth thing",
+                "fifth thing",
+                "sixth thing",
+                "seventh thing",
+                "first thing",
+                "Second thing",
+                "third thing",
+                "fourth thing",
+                "fifth thing",
+                "sixth thing",
+                "seventh thing",
+                "first thing",
+                "Second thing",
+                "third thing",
+                "fourth thing",
+                "fifth thing",
+                "sixth thing",
+                "seventh thing"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(
+                Arrays.asList(forecastArray)
+        );
+
+        ArrayAdapter<String> mForecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textview,
+                        weekForecast
+                );
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+}
